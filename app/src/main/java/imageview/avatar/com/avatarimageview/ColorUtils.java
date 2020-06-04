765cbd7e7783c30c7e9cd5faf4adc568895a08ee
Indra Rana <indra.rana>
2020-06-04 12:24:49 +0545

    Fixes Random number generated to SecureRandom generator

--- app/src/main/java/imageview/avatar/com/avatarimageview/ColorUtils.java
+++ app/src/main/java/imageview/avatar/com/avatarimageview/ColorUtils.java
@@ -2,12 +2,16 @@ package imageview.avatar.com.avatarimageview;
 
 import android.graphics.Color;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 public class ColorUtils {
 
     public static int getRandomColor() {
-        Random rnd = new Random();
-        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        SecureRandom secureRandom = new SecureRandom();
+        return Color.argb(secureRandom.nextInt(256),
+                secureRandom.nextInt(256),
+                secureRandom.nextInt(256),
+                secureRandom.nextInt(256));
     }
 }
